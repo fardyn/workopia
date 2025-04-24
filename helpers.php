b1a2ffba88f5c5cf9e972996f2bb5958091a6b04
fardyn <your-email@example.com>
2025-04-24 19:10:13 +0330

    split UI to partials

--- helpers.php
+++ helpers.php
@@ -11,3 +11,44 @@ function basePath($path = "")
 {
   return __DIR__ . "/" . $path;
 }
+
+
+/**
+ * Load a views
+ * 
+ * @param string $name
+ * @return void
+ * 
+ */
+
+function loadView($name)
+{
+  $loadViewPath = basePath("views/{$name}.view.php");
+
+  if (file_exists($loadViewPath)) {
+    require $loadViewPath;
+  } else {
+    echo "View not found '{$name}'!";
+  }
+}
+
+
+
+
+/**
+ * Load a views
+ * 
+ * @param string $name
+ * @return void
+ * 
+ */
+function loadPartial($name)
+{
+  $partailPath = basePath("views/partials/{$name}.php");
+
+  if (file_exists($partailPath)) {
+    require $partailPath;
+  } else {
+    echo "Partial not found '{$name}'!";
+  }
+}
