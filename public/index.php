6e40466940b0ef8a68fc97781adf3ae5a7ba42cd
fardyn <your-email@example.com>
2025-04-30 04:49:12 +0330

    add some routs to project

--- public/index.php
+++ public/index.php
@@ -1,4 +1,18 @@
 <?php
 require "../helpers.php";
 
-loadView("home");
+$routs = [
+  "/" => "controllers/home.php",
+  "/listings" => "controllers/listings/index.php",
+  "/listings/create" => "controllers/listings/create.php",
+  "404" => "controllers/errors/404.php"
+];
+
+$uri = $_SERVER["REQUEST_URI"];
+
+
+if (array_key_exists($uri, $routs)) {
+  require(basePath($routs[$uri]));
+} else {
+  require(basePath($routs["404"]));
+}
